c5bdf61a2be78121ba7baf41ebbe121b5f0219b3
Cico <vdacicinho@gmail.com>
2019-05-25 14:39:25 -0300

    Cargo
    Criação da tela cargo

---
+++ src/model/vo/Cargo.java
@@ -0,0 +1,23 @@
+package model.vo;
+
+public class Cargo {
+	private int id;
+	private String cargo;
+	
+	public Cargo(String cargo) {
+		this.cargo = cargo;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+}
